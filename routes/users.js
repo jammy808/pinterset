fd78078d21fb80af5d2546c9b0d072e32c2f4b7f
jammy808 <134802610+jammy808@users.noreply.github.com>
2024-01-26 19:13:52 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ const plm = require('passport-local-mongoose');
 
 const uri = "mongodb+srv://soham:soham@cluster1.4rc4s.mongodb.net/pintrest?retryWrites=true&w=majority";
 
-mongoose.connect(uri);
+mongoose.connect("mongodb://127.0.0.1:27017/pintersetDB");
 
 const userSchema = mongoose.Schema({
   usernamme : String,

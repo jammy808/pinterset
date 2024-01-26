ccab213c2dc5b0ed79bef30d050bc9e08110ccb7
soham <soahm@gmail.com>
2024-01-26 19:05:39 +0530

    db change

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const plm = require('passport-local-mongoose');
 
-const uri = "mongodb+srv://soham:soham@cluster1.rwmuwaw.mongodb.net/";
+const uri = "mongodb+srv://soham:soham@cluster1.4rc4s.mongodb.net/pintrest?retryWrites=true&w=majority";
 
 //mongoose.connect("mongodb://127.0.0.1:27017/pintersetDB");
 
